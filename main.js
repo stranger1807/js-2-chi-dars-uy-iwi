32a226007d6f3efb05faaa3065bbd7eb8fc61cc9
Shuhrat <shuhratjalolov2022@gmail.com>
2022-12-26 08:20:58 +0300

    js 2 chi dars uy iwi

---
+++ main.js
@@ -0,0 +1,71 @@
+
+var gName = prompt('Ismingizni kiriting')
+var gAge = prompt('Yoshingizni kiriting')
+alert('Salom ' + gName + '. Sizga matematik misollar beramiz togri javob berishga harakat qiling.')
+var pr = prompt('6 + 3 = ?');
+var pr1 = prompt('6 * 3 = ?');
+var pr2 = prompt('6 / 3 = ?');
+var pr3 = prompt('6 - 3 = ?');
+var pr4 = prompt('8 - 5 = ?');
+var pr5 = prompt('15 + 25 = ?');
+var pr6 = prompt('15 % 3 = ?');
+var pr7 = prompt('25 * 3 = ?');
+var pr8 = prompt('30 / 3 = ?');
+var pr9 = prompt('15 / 3 = ?');
+var pr10 = prompt('44 - 22 = ?');
+alert(gName + ' javobingiz uchun rahmat. Javobni korish uchun kansolni oching')
+if (pr == '9') {
+    console.log('Sizning javobingiz togri');
+} else {
+    console.log('Sizning javobingiz notogri. Togri javob '+(6+3));
+} 
+if (pr1 == '18') {
+    console.log('Sizning javobingiz togri');
+} else {
+    console.log('Sizning javobingiz notogri. Togri javob '+(6*3));
+} 
+if (pr2 == '2') {
+    console.log('Sizning javobingiz togri');
+} else {
+    console.log('Sizning javobingiz notogri. Togri javob '+(6 / 3));
+} 
+if (pr3 == '3') {
+    console.log('Sizning javobingiz togri');
+} else {
+    console.log('Sizning javobingiz notogri. Togri javob '+(6 - 3));
+} 
+if (pr4 == '3') {
+    console.log('Sizning javobingiz togri');
+} else {
+    console.log('Sizning javobingiz notogri. Togri javob '+(8 - 5));
+} 
+if (pr5 == '40') {
+    console.log('Sizning javobingiz togri');
+} else {
+    console.log('Sizning javobingiz notogri. Togri javob '+(15 + 25));
+} 
+if (pr6 == '0') {
+    console.log('Sizning javobingiz togri');
+} else {
+    console.log('Sizning javobingiz notogri. Togri javob '+(15 % 3));
+} 
+if (pr7 == '75') {
+    console.log('Sizning javobingiz togri');
+} else {
+    console.log('Sizning javobingiz notogri. Togri javob '+(25 * 3));
+} 
+if (pr8 == '10') {
+    console.log('Sizning javobingiz togri');
+} else {
+    console.log('Sizning javobingiz notogri. Togri javob '+(30 / 3));
+} 
+if (pr9 == '5') {
+    console.log('Sizning javobingiz togri');
+} else {
+    console.log('Sizning javobingiz notogri. Togri javob '+(15 / 3));
+} 
+if (pr10 == '22') {
+    console.log('Sizning javobingiz togri');
+} else {
+    console.log('Sizning javobingiz notogri. Togri javob '+(44 - 22));
+} 
